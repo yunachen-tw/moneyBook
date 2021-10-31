54302ae1849354ba30988efb776ecf194c17c0b2
yunachen <yunachen.tw@gmail.com>
2021-10-31 16:56:41 +0800

    rename domain name to com.yishan.moneybook

--- src/main/java/com/yishan/moneybook/SpringBootHelloWorld.java
+++ src/main/java/com/yishan/moneybook/SpringBootHelloWorld.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.yishan.moneybook;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
