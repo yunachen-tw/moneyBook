b447234c9f968dad0418b92ddff8446adb133dac
yunachen <yunachen.tw@gmail.com>
2021-11-07 16:16:34 +0800

    add testing data instead of database

--- src/main/java/com/yishan/moneybook/record/RecordController.java
+++ src/main/java/com/yishan/moneybook/record/RecordController.java
@@ -1,5 +1,9 @@
 package com.yishan.moneybook.record;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 
 import org.springframework.http.MediaType;
@@ -16,6 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/records", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RecordController {
+    // testing data list
+    private final List<Record> recordDB = new ArrayList<>();
+
+    @PostConstruct
+    private void initDB() {
+        recordDB.add(new Record(0, 90, "牛肉麵", "2021/10/25", "牛肉麵好吃"));
+        recordDB.add(new Record(1, 60, "鍋貼", "2021/10/27", "八方雲集"));
+        recordDB.add(new Record(2, 120, "小火鍋", "2021/11/01", "牛肉麵好吃"));
+        recordDB.add(new Record(3, 65, "蘿蔔糕套餐", "2021/11/03", "cp值很高！"));
+        recordDB.add(new Record(4, 150, "大補鐵板燒", "2021/11/04", "炒牛肉+高麗菜+飯"));
+    }
 
     // 取得所有紀錄
     @GetMapping
